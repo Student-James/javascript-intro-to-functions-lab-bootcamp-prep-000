451a76ea21919068016da8eaeec91a194773b4af
james.head@mac.com <james.head@mac.com>
2018-08-15 22:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ console.log ("HELLO")
     
     if  (lowercase.toLowerCase() === lowercase)
     { console.log ("I can\'t hear you!") }
-    
     else (uppercase.toUpperCase() === uppercase)
     { console.log ("YES INDEED!") }
   }
