2dc61d0d62576a41da1c22ad798fc90b99e6a9eb
james.head@mac.com <james.head@mac.com>
2018-08-15 22:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ console.log ("HELLO")
  
   function sayHiToGrandma(string) {
     
-    if  (lowercase.toLowerCase() === lowercase)
+    if  (string.toLowerCase() === string)
     { return ("I can\'t hear you!") }
-    else if (uppercase.toUpperCase() === uppercase)
+    else if (string.toUpperCase() === string)
     { return ("YES INDEED!") }
     else ("I love you, Grandma.")
     {return ("I love you, too.")}
