c57e57c3d13e9160a9f5a28ca34474817c8e5723
james.head@mac.com <james.head@mac.com>
2018-08-15 21:32:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logshout (string) {
+  console.log ('logshout')
+  return string.toUpperCase()
+}
